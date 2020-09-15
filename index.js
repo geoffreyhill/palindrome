ddd1fe2c090f2729f231785327105bff738afdba
Geoffrey Hill <geoff@49digital.com>
2020-09-15 21:11:32 +0100

    Initialize repository

---
+++ index.js
@@ -0,0 +1,38 @@
+// Reverses a string
+function reverse(string) {
+  return string.split("").reverse().join("");
+}
+
+
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns content processed for palindrome testing.
+
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
+
+  // Returns true for a palindrome, false otherwise
+
+  this.palindrome = function palindrome() {
+    return this.processedContent() === reverse(this.processedContent());
+  }
+}
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns content processed for palindrome testing.
+
+  this.processedContent = function processedContent() {
+    return this.translation.toLowerCase();
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
